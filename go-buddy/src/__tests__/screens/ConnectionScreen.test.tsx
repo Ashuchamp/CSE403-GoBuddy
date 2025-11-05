9aeb79765a9caf629655f3903f30161904f9b71f
sjn0305 <sjn0305@uw.edu>
2025-11-05 11:19:31 -0800

    fix tests

--- go-buddy/src/__tests__/screens/ConnectionScreen.test.tsx
+++ go-buddy/src/__tests__/screens/ConnectionScreen.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import {ConnectionsScreen} from '../../screens/ConnectionsScreen';
 import {User} from '../../types';
 
